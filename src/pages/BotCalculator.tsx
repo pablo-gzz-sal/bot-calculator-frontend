64049204924ccec09a65abca18939afeee715fe5
Esencial360 <esencial360yoga@gmail.com>
2025-01-26 00:52:05 +0100

    develop: added a bit of styling to bot calculator

--- src/pages/BotCalculator.tsx
+++ src/pages/BotCalculator.tsx
@@ -5,32 +5,31 @@ import IHistoryItem from "../models/HistoryItem.ts";
 import { sendUserCommand } from "../services/ChatInput.service.ts";
 import { getHistory } from "../services/history.service.ts";
 import socket from "../services/Socket.service.ts";
+import '../styles/BotCalulator.css'
 
 const BotCalculator: React.FC = () => {
   const [historyList, setHistoryList] = useState<IHistoryItem[]>([]);
 
   useEffect(() => {
     const handleHistoryLoaded = (history: IHistoryItem[]) => {
-      setHistoryList(history) 
+      setHistoryList(history);
     };
-    socket.on("history_loaded", handleHistoryLoaded)
+    socket.on("history_loaded", handleHistoryLoaded);
 
     const handleHistoryItem = (newItem: IHistoryItem) => {
-      setHistoryList((oldList: IHistoryItem[]) => [newItem, ...oldList])
-    }
-    socket.on("history_item", handleHistoryItem)
+      setHistoryList((oldList: IHistoryItem[]) => [newItem, ...oldList]);
+    };
+    socket.on("history_item", handleHistoryItem);
 
     return () => {
-      socket.off("history_loaded", handleHistoryLoaded)
-      socket.off("history_item", handleHistoryItem)
-    }
-
+      socket.off("history_loaded", handleHistoryLoaded);
+      socket.off("history_item", handleHistoryItem);
+    };
   }, []);
 
   useEffect(() => {
     socket.emit("load_history");
-    console.log('loading history');
-    
+    console.log("loading history");
   }, []);
 
   const handleUserCommand = async (command: string) => {
@@ -48,11 +47,20 @@ const BotCalculator: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>Bot Calculator</h1>
-      <ChatInput onSend={handleUserCommand} />
-      <h2>History List</h2>
-      <HistoryList history={historyList} />
+    <div className="container">
+      <header className="header">
+        <h1>Bot Calculator</h1>
+        <p>A simple bot for performing calculations</p>
+      </header>
+
+      <section className="chat-input">
+        <ChatInput onSend={handleUserCommand} />
+      </section>
+
+      <section className="history">
+        <h2>History List</h2>
+        <HistoryList history={historyList} />
+      </section>
     </div>
   );
 };
